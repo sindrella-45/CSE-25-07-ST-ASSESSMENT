42fa7aab61b30dea3bb199bfbd34449d3ebf2115
sindrella-45 <sindrellaacen512@gmail.com>
2025-10-17 13:09:02 +0300

    created html files

--- Acen_Sindrella/assessment/assessment/urls.py
+++ Acen_Sindrella/assessment/assessment/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ref import views
-from ref.views import(login)
+from ref.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
